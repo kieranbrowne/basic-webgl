b9015055b61a1f30087668ac35033e23041d7fce
Kieran Browne <kierantbrowne@gmail.com>
2018-05-30 16:57:11 +1000

    added layers

--- net.py
+++ net.py
@@ -11,13 +11,29 @@ Y = tf.constant(
 
 sess = tf.Session()
 
-W0 = tf.Variable([[0.3, -0.1], [-0.2, 0.15]])
-sess.run(W0.initializer)
-b0 = tf.Variable([0.0, 0.0])
-sess.run(b0.initializer)
+# layer 1
+W1 = tf.Variable([[0.3, -0.1], [-0.2, 0.15]])
+sess.run(W1.initializer)
+b1 = tf.Variable([0.0, 0.0])
+sess.run(b1.initializer)
+L1 = tf.nn.relu(tf.add(tf.matmul(X, W1), b1))
+
+# layer 2
+W2 = tf.Variable([[0.3, -0.1], [-0.2, 0.15]])
+sess.run(W2.initializer)
+b2 = tf.Variable([0.0, 0.0])
+sess.run(b2.initializer)
+L2 = tf.nn.relu(tf.add(tf.matmul(X, W2), b2))
+
+# layer 3
+W3 = tf.Variable([[0.3, -0.1], [-0.2, 0.15]])
+sess.run(W3.initializer)
+b3 = tf.Variable([0.0, 0.0])
+sess.run(b3.initializer)
+L3 = tf.nn.relu(tf.add(tf.matmul(X, W3), b3))
 
 
-pred = tf.nn.relu(tf.add(tf.matmul(X, W0), b0))
+pred = L3
 
 cost = tf.reduce_mean(tf.pow(pred-Y, 2))
 
